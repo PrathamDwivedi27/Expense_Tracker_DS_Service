24b519aee62b7299565a98534ab8b7ca0ec404e0
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-30 07:45:31 +0530

    [Feat]: DS Service is completed

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -12,7 +12,6 @@ def handle_message():
     message=request.json.get('message')     # humein jo info aayegi wo aise aayegi {"message": "you have spent 1000 with your credit card bill"}
     result=messageService.process_message(message)
     if result:
-        print("Result from LLM:", jsonify(result.model_dump()))
         return jsonify(result.model_dump()),200
     else:
         return jsonify({"error": "Message is not a bank SMS"}), 400
