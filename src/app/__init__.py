b1a693aad9badf7314f4f8110c96aaf6f18f6d0f
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-30 07:26:14 +0530

    [Feat]: Complete init file

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from flask import request,jsonify
+from service.messageService import MessageService
 
 app=Flask(__name__)
 app.config.from_pyfile('config.py')
 
+messageService= MessageService()
+
 @app.route('/v1/ds/message', methods=['POST'])
 def handle_message():
     message=request.json.get('message')     # humein jo info aayegi wo aise aayegi {"message": "you have spent 1000 with your credit card bill"}
+    result=messageService.process_message(message)
+    if result:
+        return result
+    else:
+        return None
     
+@app.route('/',methods=['GET'])
+def handle_endpoint():
+    print("My DS Service is working")
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
